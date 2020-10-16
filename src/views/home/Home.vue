e3d29788c4d318f3476dd976e10169834d88400e
chengyijun <cyjmmy@foxmail.com>
2020-10-16 22:59:59 +0800

    封装NavBar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
+        <nav-bar class="home-navbar">
+            <div slot="center">蘑菇街</div>
+        </nav-bar>
         <h1>home</h1>
     </div>
 </template>
 
 <script>
+    import NavBar from "components/common/navbar/NavBar";
     export default {
-        name: "Home"
+        name: "Home",
+        components: {
+            NavBar
+        }
     }
 </script>
 
 <style scoped>
-
+    .home-navbar{
+        background-color: var(--color-text);
+        color: var(--color-background);
+    }
 </style>
