e82ef3646a9a7a9572b8322d683f538282476c72
chengyijun <cyjmmy@foxmail.com>
2020-10-16 21:58:57 +0800

    导入了已经提前写好的TabBar

---
+++ src/views/shopcart/Shopcart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>shopcart</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Shopcart"
+    }
+</script>
+
+<style scoped>
+
+</style>
